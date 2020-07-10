efe58fb045cb14d1936353e5921d15311ba56036
pea-xwq <812991655@qq.com>
2020-07-10 16:16:59 +0800

    config先改回来吧

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 DEBUG = True
 SECRET_KEY = os.urandom(24)
 
-HOSTNAME = '192.168.1.9'
+HOSTNAME = '127.0.0.1'
 PORT = '3306'
 DATABASE = 'pcmp1'
-USERNAME = 'abc'
-PASSWORD = '111'
+USERNAME = 'root'
+PASSWORD = '993961218'
 DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
 SQLALCHEMY_DATABASE_URI = DB_URI
