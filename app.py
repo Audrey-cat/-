630808af31e31277322f269675b361e2bdd78528
pea-xwq <812991655@qq.com>
2020-07-11 13:52:38 +0800

    新增注册功能

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from datetime import timedelta
 import pymysql
 import config
 from exts import db
+from models import User
+
 app = Flask(__name__)
 # app.secret_key="123"
 app.config.from_object(config)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = timedelta(seconds=1)
 db.init_app(app)
 var=[]
+
 
 @app.route('/')
 def hello_world():
@@ -23,7 +26,6 @@ def login():
     if request.method == 'GET':
         return render_template('login.html')
     else:
-        #pass
         session.permanent= True
         return redirect(url_for('hello_world'))
 
@@ -32,8 +34,25 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')
     else:
-        pass
-        return redirect(url_for('login'))
+        telephone = request.form.get('telephone')
+        username = request.form.get('username')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        #手机号码验证，如果被注册了，就不能再注册
+        user = User.query.filter(User.telephone == telephone).first()
+        if user:
+            return u'手机号码已被注册，请更换！'
+        else:
+            # 两次密码不相等
+            if password1 != password2:
+                return u'两次密码不相等，请核对后再填写！'
+            else:
+                user = User(telephone=telephone,username=username,password=password1)
+                db.session.add(user)
+                db.session.commit()
+                # 注册成功，跳转到登录界面
+                return redirect(url_for('login'))
 
 @app.route('/schoolQuery', methods=['GET', 'POST'])
 def schoolQuery():
@@ -42,12 +61,15 @@ def schoolQuery():
     else:
         pass
 
+
 @app.route('/catQuery', methods=['GET', 'POST'])
 def catQuery():
     if request.method == 'GET':
         return render_template('catQuery.html')
     else:
         pass
+
+
 @app.context_processor
 def my_context_processor():
     user=0
